20903050a8e85d22b1cbc5973499bf91547fd524
Daniel Pratt <daniel@blaumagier.com>
2017-04-03 14:11:26 -0700

    Created class for cell and began setting up Table View.

--- On the Map/OTMTableOfPinsViewController.swift
+++ On the Map/OTMTableOfPinsViewController.swift
@@ -10,36 +10,33 @@ import UIKit
 import MapKit
 
 class OTMTableOfPinsViewController: UITableViewController {
+    
+    // MARK: - Properties
+    
+    // Model
+    var tableData = [OTMMapData]()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        tableData = OTMClient.sharedInstance().mapPinData!
+        self.tableView.reloadData()
     }
     
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return tableData.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "OTMDataCell", for: indexPath)
 
-        // Configure the cell...
+        print("Cell number \(String(describing: indexPath.row))")
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
---
+++ On the Map/OTMTableofPinsViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  OTMTableofPinsViewCell.swift
+//  On the Map
+//
+//  Created by Daniel Pratt on 4/3/17.
+//  Copyright © 2017 Daniel Pratt. All rights reserved.
+//
+
+import UIKit
+
+class OTMTableofPinsViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
